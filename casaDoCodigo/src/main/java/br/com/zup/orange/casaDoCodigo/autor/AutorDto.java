5d7a3a46a4a8b3d5f600870faaa590e589627123
jpauloleao <jpauloazv@gmail.com>
2021-06-29 14:25:49 -0300

    Funcionalidade Email Unico

--- casaDoCodigo/src/main/java/br/com/zup/orange/casaDoCodigo/autor/AutorDto.java
+++ casaDoCodigo/src/main/java/br/com/zup/orange/casaDoCodigo/autor/AutorDto.java
@@ -6,6 +6,8 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import br.com.zup.orange.casaDoCodigo.validacao.UniqueValue;
+
 
 public class AutorDto {
 
@@ -14,13 +16,14 @@ public class AutorDto {
 	
 	@Email(message = "Digite um email válido!")
 	@NotBlank(message =  "Não pode ser vazio")
+	@UniqueValue(domainClass = Autor.class,fieldName = "email")
 	private String email;
 	
 	@Size(max = 400)
 	private String descricao;
 
 	public AutorDto(@NotBlank(message = "Não pode ser vazio!") String nome,
-			@Email(message = "Digite um email válido!") @NotBlank(message = "Não pode ser vazio") String email,
+			@Email(message = "Digite um email válido!") @NotBlank(message = "Não pode ser vazio")  String email,
 			@Size(max = 400) String descricao) {
 		this.nome = nome;
 		this.email = email;
